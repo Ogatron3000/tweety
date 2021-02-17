18c87c14c16bf3c237963a651da96704954004ba
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2021-02-17 15:46:09 +0100

    routes and views tweaks

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/tweets';
+    public const HOME = 'home/tweets';
 
     /**
      * The controller namespace for the application.
