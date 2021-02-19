404fa2eb78198f5b10e2c17e664111da49985ff4
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2021-02-19 14:03:34 +0100

    removed profile link underline on friend profiles

--- resources/views/_links.blade.php
+++ resources/views/_links.blade.php
@@ -1,11 +1,12 @@
 <div class="p-4 w-1/6 bg-white border-b border-gray-200 rounded-xl">
+    {{ request()->routeIs() }}
     <x-nav-link :href="route('tweets.index')" :active="request()->routeIs('tweets.index')">
         {{ __('Home') }}
     </x-nav-link>
     <x-nav-link :href="route('explore')" :active="request()->routeIs('explore')">
         {{ __('Explore') }}
     </x-nav-link>
-    <x-nav-link :href="route('profiles.show', auth()->user()->username)" :active="request()->routeIs('profiles.show')">
+    <x-nav-link :href="route('profiles.show', auth()->user()->username)" :active="request()->is('profiles/'.auth()->user()->username)">
         {{ __('Profile') }}
     </x-nav-link>
     <x-nav-link>
